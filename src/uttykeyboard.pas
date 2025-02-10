4405890286aac9dfb860307e25e304a63016f544
neuts-jl <jeanlucneuts62@gmail.com>
2025-02-10 18:09:37 +0100

    Update uttykeyboard.pas

--- src/uttykeyboard.pas
+++ src/uttykeyboard.pas
@@ -1,30 +1,30 @@
-{
-  *****************************************************************************
-   Unit        : uttykeyboard
-   Author      : NEUTS JL
-   License     : GPL (GNU General Public License)
-   Date        : 01/02/2025
+  {
+    *****************************************************************************
+     Unit        : uttykeyboard
+     Author      : NEUTS JL
+     License     : GPL (GNU General Public License)
+     Date        : 01/02/2025
 
-   Description : This unit handles keyboard management in console mode
+     Description : This unit handles keyboard management in console mode
 
-   WARNING     : This program does not use the CRT unit, because it disrupts
-                 the proper functioning of the console, especially for launched
-                 shells.
+     WARNING     : This program does not use the CRT unit, because it disrupts
+                   the proper functioning of the console, especially for launched
+                   shells.
 
-   This program is free software: you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the Free
-   Software Foundation, either version 3 of the License, or (at your option)
-   any later version.
+     This program is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the Free
+     Software Foundation, either version 3 of the License, or (at your option)
+     any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-   Public License for more details.
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+     Public License for more details.
 
-   You should have received a copy of the GNU General Public License along with
-   this program. If not, see <https://www.gnu.org/licenses/>.
-  *****************************************************************************
-}
+     You should have received a copy of the GNU General Public License along with
+     this program. If not, see <https://www.gnu.org/licenses/>.
+    *****************************************************************************
+  }
 unit uttykeyboard;
 
 {$mode ObjFPC}{$H+}
